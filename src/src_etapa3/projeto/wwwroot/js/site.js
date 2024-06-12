2997d4774c6325796fcb0003015ff15ba258749f
mailsonpuc <mailson.costa@proton.me>
2024-06-12 00:27:06 -0300

    acessibilidade cores

--- src/src_etapa3/projeto/wwwroot/js/site.js
+++ src/src_etapa3/projeto/wwwroot/js/site.js
@@ -2,3 +2,24 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+const asc_escuro = document.getElementById("esc_escuro");
+const nav = document.querySelector("nav");
+
+
+
+asc_escuro.addEventListener("click", myFunction);
+function myFunction() {
+
+    if(nav.className.includes("bg-white")) {
+     
+      document.querySelector("nav").className="navbar navbar-expand-sm navbar-toggleable-sm navbar-light bg-info bg-gradient border-bottom box-shadow mb-3";
+    }
+    
+    
+    asc_escuro.innerHTML='visibility_on';
+    document.body.style.backgroundColor = "#696969";
+
+   
+    
+}
